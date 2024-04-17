9ad53f847149bcaf891874856dffe46bd8c6a5df
JSebastianPaezSalesianoDev <usr3766@salesianos-lacuesta.net>
2024-04-17 10:16:16 +0100

    Creamos la clase menu, con un metodo estatico que retorna un String con nuestro menu

--- src/App.java
+++ src/App.java
@@ -1,5 +1,8 @@
+import net.ripadbaisor.menus.Menu;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        System.out.println(Menu.menu());
     }
 }
